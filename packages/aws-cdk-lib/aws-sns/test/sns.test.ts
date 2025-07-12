d1cc5aa368949b3162f23cb39c6ccb25c588ef0b
Tsukada.Masaki <licuxyx5z@gmail.com>
2025-07-12 05:51:24 +0000

    feat(sns): add displayName property to a Topic

--- packages/aws-cdk-lib/aws-sns/test/sns.test.ts
+++ packages/aws-cdk-lib/aws-sns/test/sns.test.ts
@@ -791,4 +791,17 @@ describe('Topic', () => {
       });
     });
   });
+
+  test('specify displayName', () => {
+    const stack = new cdk.Stack();
+
+    new sns.Topic(stack, 'MyTopic', {
+      displayName: 'MyDisplayName',
+    });
+
+    Template.fromStack(stack).hasResourceProperties('AWS::SNS::Topic', {
+      DisplayName: 'MyDisplayName',
+    });
+  });
+  
 });
